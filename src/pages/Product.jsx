29b9af168e35ab338288d3052e381181bdf847cc
Mandeep Narotta <narotta@gmail.com>
2024-01-21 15:32:24 +0530

    deploy

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,25 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import ProductInfo from '../components/ProductInfo'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Product() {
   const { ProductId } = useParams();
-  const [info,setinfo]=useState(null)
-  let getProductInfo=async ()=>{
+  const [info, setinfo] = useState(null)
+  // let getProductInfo = 
+  useCallback(
+    async () => {
     axios.get(`${process.env.REACT_APP_API}api/getProductInfo/${ProductId}`).then((Response) => {
       if (Response.data.data.success) {
         // setFeaturedItem(Response.data.data.data)
         setinfo(Response.data.data.data)
       }
-  }).catch((err) => {
-        
+    }).catch((err) => {
+
       console.log(err)
-  })
-  }
-  useEffect(()=>{
-      getProductInfo();
-  },[getProductInfo])
+    })
+  }, [ProductId])
+  // useEffect(()=>{
+  //     getProductInfo();
+  // },[getProductInfo])
   return (
     <div>
       <ProductInfo info={info} ></ProductInfo>
